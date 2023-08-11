a3cc7166e435e58af134147ba47d4b1f32a381e1
Mend1s <andremendesskt99@gmail.com>
2023-08-11 01:55:47 -0300

    ajustes

--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -1,7 +1,7 @@
+import { Client } from 'src/app/features/cliente/models/client';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Client } from '../features/cliente/models/client';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +10,26 @@ export class ClienteService {
 
   constructor(private http: HttpClient) { }
 
-  private baseUrl = 'http://localhost:3000'
+  private baseUrl = 'http://localhost:3000/'
 
-  getListClients(): Observable<Client[]>{
-    return this.http.get<Client[]>(`${this.baseUrl}/client`)
+  getListClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(`${this.baseUrl}client`)
   }
 
-  getClientById(id: string): Observable<Client>{
-    return this.http.get<Client>(`${this.baseUrl}/client/${id}`)
+  getClientById(id: string): Observable<Client> {
+    return this.http.get<Client>(`${this.baseUrl}client/${id}`)
   }
 
-  putClient(client: Client): Observable<any>{
+  putClient(client: Client): Observable<any> {
     return this.http.put(`${this.baseUrl}client/${client.id}`, client);
   }
+
+  postClient(client: Client): Observable<any> {
+    return this.http.post(`${this.baseUrl}client/`, client);
+  }
+
+  deleteClient(id: string): Observable<any> {
+    return this.http.delete<Client>(`${this.baseUrl}client/${id}`)
+  }
 }
+
